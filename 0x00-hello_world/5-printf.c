f73d01d0b934a04b70acc326a49fd02069709b15
faithmine <lajumine@yahoo.com>
2023-02-19 16:39:24 -0800

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-nakkcbkjczxhciuddj
+#include <studio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
