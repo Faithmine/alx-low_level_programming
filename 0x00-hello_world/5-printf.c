d7c41f559dfc07869d1ae436b0352b8a0a5bc8bf
faithmine <lajumine@yahoo.com>
2023-02-20 09:04:12 -0800

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,10 @@
 
 /**
  * main - Print a string
+ *Description: use printf
  * Return: Always 0
  */
+
 int main(void)
 {
 	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
