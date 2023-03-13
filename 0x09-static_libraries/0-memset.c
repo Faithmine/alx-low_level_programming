7a6fb1e83e493ebba4988ce2883ab16bc5b8bdbe
faithmine <lajumine@yahoo.com>
2023-03-13 02:59:06 -0700

    added files

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+* a function that fills memory with a constant byte.
+*@s: pointer to n
+*@n: bytes of the memory area
+*@b: constant byte
+*Return: s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
